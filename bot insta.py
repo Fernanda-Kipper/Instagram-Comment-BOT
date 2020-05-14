0e498dcae72112a39980afb00caae40f47d5776b
Fernanda Kipper Bucoski de Sousa <61896274+Fernanda-Kipper@users.noreply.github.com>
2020-05-13 21:05:59 -0300

    Update version

--- bot insta.py
+++ bot insta.py
@@ -12,21 +12,9 @@ comment = ""  # last but not least, inside this quotation marks you should write
 # bellow here, insert the path way to the Gecko driver that you had install, including .exe file
 driver = webdriver.Firefox(executable_path=r'path\to\your\geckodriver.exe')
 driver.implicitly_wait(5)  # waiting 5 seconds to load
-initializingTheBrowser()
+#after this, don't need more changes, just run the programm
 
-def initializingTheBrowser():
-    driver.get('https: put here the instagram post link')  # link of the post ypu want to interact
-    sleep(5)  # waiting 5 seconds to load the page
-    login(username,password)
-
-def login(username, password):
-    # after successfully loading the web page, the bot will start login in
-    driver.find_element_by_css_selector('button.L3NKy').click()
-    driver.find_element_by_css_selector("input[name='username']").send_keys(username)
-    driver.find_element_by_css_selector("input[name='password']").send_keys(password)
-    driver.find_element_by_css_selector('div.Igw0E:nth-child(4) > button:nth-child(1)').click()
-    sleep(5)  # waiting 5 seconds to load the page
-    commenting(comment)
+#bot time to work:
 
 def commenting(comment):
     # after successfully login in, the bot will start commenting, infinite loop, to stop, just close the page to stop
@@ -39,3 +27,27 @@ def commenting(comment):
 """"if you want to automatically close the page after N numbers of comments, you can just start a FOR LOOP in range(N):
 after insert the code above (the one inside of the while, responsible to comment) and then, outside the FOR LOOP, add:
 driver.close()"""
+
+def login(username, password):
+    # after successfully loading the web page, the bot will start login in
+    driver.find_element_by_css_selector('button.L3NKy').click()
+    driver.find_element_by_css_selector("input[name='username']").send_keys(username)
+    driver.find_element_by_css_selector("input[name='password']").send_keys(password)
+    driver.find_element_by_css_selector('div.Igw0E:nth-child(4) > button:nth-child(1)').click()
+    sleep(5)  # waiting 5 seconds to load the page
+    commenting(comment)
+
+def initializingTheBrowser():
+    driver.get('https: put here the instagram post link')  # link of the post ypu want to interact
+    sleep(5)  # waiting 5 seconds to load the page
+    login(username, password)
+
+if __name__ == '__main__':
+    initializingTheBrowser()
+    #initializing the program
+
+
+
+
+
+
